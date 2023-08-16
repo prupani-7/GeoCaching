07973233b237ec896cf6972e9f6553318e676ca5
Priyanka Rupani <prupani@esri.com>
2023-08-16 12:09:06 -0700

    hello world

---
+++ app/src/main/java/com/example/geocachingapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.geocachingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val featureService = ServiceFea
+    }
+}
